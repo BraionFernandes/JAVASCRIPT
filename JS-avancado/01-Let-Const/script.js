fe85fedd50bfdce6e740c38489cdbbc9678540b5
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2024-01-29 14:19:21 -0300

    java script avancado

---
+++ JS-avancado/01-Let-Const/script.js
@@ -0,0 +1,5 @@
+// AULA 01 - let E const
+
+var nome="Braion"; // "var" nunca usar que pode ocorrer vazamento de codigo para outras funções ex.
+let nome1="Tailon"; // "let" pode ser usada, e possivel alterar seu valor.
+const nome="Taisler"; // "const" não pode alterar o valor padrão dado.
