abe9408d66a4a290960d3da33bbc2f5f896a9304
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2024-02-02 15:03:19 -0300

    finalização do projeto "lista de tarefas" avançado.

--- Projetos/ToDoAvancado/js/script.js
+++ Projetos/ToDoAvancado/js/script.js
@@ -7,6 +7,8 @@ const btnEditor=document.querySelector(".inputSub");
 const btnTodas=document.getElementById("btnTodas");
 const btnCompleto=document.getElementById("btnCompleto");
 const btnAFazer=document.getElementById("btnAFazer");
+const btnFiltros=document.querySelectorAll(".btnFiltro");
+const inputSearch=document.querySelector("#pesquisa");
 const tarefasAll=[]
 btnCriar.addEventListener("click",()=>{
     if(tarefaNome.value!=""){
@@ -39,11 +41,46 @@ btnEditor.addEventListener("click",()=>{
     editor.classList.add("hidden");
 })
 btnTodas.addEventListener("click",()=>{
+    btnFiltros.forEach(el=>{
+        el.classList.remove("selecionado");
+    })
+    btnTodas.classList.add("selecionado");
     const tarefasRegistradas=document.getElementById("tarefasRegistradas");
+    tarefasRegistradas.innerHTML="";
+    console.log(tarefasAll)
+    for(let i=0;i<tarefasAll.length;i++){
+        tarefasRegistradas.appendChild(tarefasAll[i]);
+    };
 })
 btnCompleto.addEventListener("click",()=>{
+    btnFiltros.forEach(el=>{
+        el.classList.remove("selecionado");
+    })
+    btnCompleto.classList.add("selecionado");
     const tarefasRegistradas=document.getElementById("tarefasRegistradas");
+    tarefasRegistradas.innerHTML="";
+    const tarefasCompleto=tarefasAll.filter(tarefa => tarefa.classList.contains('completo'));
+    for(let i=0;i<tarefasCompleto.length;i++){
+        tarefasRegistradas.appendChild(tarefasCompleto[i]);
+    };
 })
 btnAFazer.addEventListener("click",()=>{
+    btnFiltros.forEach(el=>{
+        el.classList.remove("selecionado");
+    })
+    btnAFazer.classList.add("selecionado");
     const tarefasRegistradas=document.getElementById("tarefasRegistradas");
+    tarefasRegistradas.innerHTML="";
+    const tarefasAFazer=tarefasAll.filter(tarefa => tarefa.classList.contains('aFazer'));
+    for(let i=0;i<tarefasAFazer.length;i++){
+        tarefasRegistradas.appendChild(tarefasAFazer[i]);
+    };
 })
+inputSearch.oninput=()=>{
+    tarefasRegistradas.innerHTML="";
+
+    tarefasAll.filter((item)=>
+    item.innerText.toLowerCase().includes(inputSearch.value.toLowerCase())
+    )
+    .forEach((item)=>tarefasRegistradas.appendChild(item));
+}
